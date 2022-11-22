c8f6a796107f020cb726e8270c85dd8b29053d70
Lucas Ignacio <lirodriguez@fi.uba.ar>
2022-11-22 03:02:33 -0300

    ReactUnidad1

---
+++ Sitio transporte/frontend/src/pages/HomePages.js
@@ -0,0 +1,17 @@
+const HomePages= (props) => {
+    return(
+        <main className="holder">
+            <div className="homeimg">
+            <img src="img/R.jpg" width="100" alt="TorneosKD"/>
+            </div>
+            <div className="columnas">
+                <div className="bienvenidos">
+                    <h2>Bienvenidos</h2>
+                </div>
+            </div>
+            
+        </main>
+    )
+}
+
+export default HomePages;
