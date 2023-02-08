f8c09603ed77375a2b2f3e8702fb0dc10d67fb29
Lucas Ignacio <lirodriguez@fi.uba.ar>
2023-02-08 02:12:23 -0300

    Errores antes de entregar final

--- Sitio Torneo React/backend/app.js
+++ Sitio Torneo React/backend/app.js
@@ -61,7 +61,7 @@ app.use("/api",cors(),apiRouter);
 //app.use(function(req, res, next) {
 //  next(createError(404));
 //});
-
+ 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
